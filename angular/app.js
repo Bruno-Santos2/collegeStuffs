0d55f9fd1615ef0f7532dac946a4de8ffaa28852
Henrique Gibin <henrique.gibin96@gmail.com>
2016-05-01 22:48:37 -0300

    refactor project

--- angular/app.js
+++ angular/app.js
@@ -1,39 +1,58 @@
 (function() {
+  var app = angular.module('Store', []);
 
-var app = angular.module('store', []);
-app.controller('StoreController', function(){
-	this.products = gems;
-});
+  app.controller('StoreController', function(){
+    this.products = gems;
+  });
 
-var gems = [
-	{
-		name: 'Dodecahedron',
-		price: 2.95,
-		description: 'Some gems have a hidden qualities	beyond their luster, beyond their shine... Dodeca is one of those gems.',
-		canPurchase: true,
-		soldOut: false,
-	},
-	{
-		name: 'Azurite',
-		price: 2.95,
-		description: 'Some gems have a hidden qualities	beyond their luster, beyond their shine... Azurite is one of those gems.',
-		canPurchase: true,
-		soldOut: false,
-	},
-	{
-		name: 'BloodStone',
-		price: 5.95,
-		description: 'Some gems have a hidden qualities	beyond their luster, beyond their shine... BloodStone is one of those gems.',
-		canPurchase: true,
-		soldOut: false,
-	},
-	{
-		name: 'Zircon',
-		price: 3.95,
-		description: 'Some gems have a hidden qualities	beyond their luster, beyond their shine... Zircon is one of those gems.',
-		canPurchase: true,
-		soldOut: false,
-	}
-];
+  app.controller('TabController', function(){
+    this.tab = 1;
 
+    this.setTab = function(newValue){
+      this.tab = newValue;
+    };
+
+    this.isSet = function(tabName){
+      return this.tab === tabName;
+    };
+  });
+
+  var gems = [
+    {
+      name: 'Azurite',
+      description: "Some gems have hidden qualities beyond their luster, beyond their shine... Azurite is one of those gems.",
+      shine: 8,
+      price: 110.50,
+      images: [
+        "images/image1.png"
+      ],
+    },
+    {
+      name: 'Bloodstone',
+      description: "Origin of the Bloodstone is unknown, hence its low value. It has a very high shine and 12 sides, however.",
+      shine: 9,
+      price: 22.90,
+      images: [
+        "images/image2.png"
+      ],
+    },
+    {
+      name: 'Zircon',
+      description: "Zircon is our most coveted and sought after gem. You will pay much to be the proud owner of this gorgeous and high shine gem.",
+      shine: 70,
+      price: 1100,
+      images: [
+        "images/image3.png"
+      ],
+    },
+    {
+	    name: 'Dodecahedron',
+	    description: 'Some gems have a hidden qualities	beyond their luster, beyond their shine... Dodeca is one of those gems.',
+	    shine: 3,
+	    price: 2.95,
+	    images: [
+	    	"images/image4.png"
+	    ]
+    }
+  ];
 })();
